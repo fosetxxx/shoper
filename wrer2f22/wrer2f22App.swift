d59c5740b6c73a92460be9c2d9f9f1768c43c2f2
Semih Karahan <karahansemih@hotmail.com>
2023-05-01 23:21:17 +0300

    Initial Commit

---
+++ wrer2f22/wrer2f22App.swift
@@ -0,0 +1,17 @@
+//
+//  wrer2f22App.swift
+//  wrer2f22
+//
+//  Created by Semih Karahan on 1.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct wrer2f22App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
